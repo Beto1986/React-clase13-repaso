c3b5de84adb6debd8974232a53d7a2e29704dc14
Alberto <albertodamianlopez@gmail.com>
2021-10-28 14:42:28 -0300

    Repaso

--- context/src/App.js
+++ context/src/App.js
@@ -1,3 +1,11 @@
+// 4- ContextAPI
+// Vamos a hacer un ejercicio que me comentaron que a algunos de ustedes no les salio:
+// En un proyecto nuevo generá un input en app.js que pida el nombre, luego generá estos componentes:
+// 	App.js llama a Fichero.js
+// Fichero.js llama a Registro.js
+// Registro.js llama a Nombre.js
+// 	Y dentro de Nombre.js generar un párrafo en donde se muestre el input ingresado. No   uses ni una prop.
+
 import "./App.css";
 import GetNombre from "./contexts/nombreContext";
 import Fichero from "./Fichero";
